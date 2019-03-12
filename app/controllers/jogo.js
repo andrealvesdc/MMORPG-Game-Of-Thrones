f5c1adfb0d9df1ff36b495a4f8a1eeaf8ad12ada
André Alves da Cruz <andrealvesdc@gmail.com>
2019-03-11 23:17:45 -0300

    Update jogo.js
    Validação de usuários para terem acesso a rotas restritas

--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -3,7 +3,7 @@ module.exports.jogo = function(application, req, res){
 	if(req.session.autorizado){
 		res.render("jogo", {img_casa: req.session.casa});
 	} else {
-		res.send('Usuário precisa fazer login');
+		res.send('O usuário precisa fazer loogin');
 	}
 }
 
